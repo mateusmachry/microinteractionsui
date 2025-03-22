21d42c64437965e843226ca0bf23c1496b01a800
Mateus <mateusmachry2016@gmail.com>
2025-03-22 00:27:09 -0300

    updated hero texts

--- src/app/(home)/components/hero.tsx
+++ src/app/(home)/components/hero.tsx
@@ -6,13 +6,13 @@ export function Hero() {
         <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-16">
             <div className="mt-5 max-w-3xl text-center mx-auto">
                 <h1 className="block text-pretty font-semibold tracking-tight text-4xl md:text-5xl lg:text-6xl bg-gradient-to-b from-foreground to-foreground/75 bg-clip-text text-transparent">
-                    Build Fluid and Elegant UIs with <AnimatedGradientText colorFrom="#D4D4D4" colorTo="#737373" className="text-4xl md:text-5xl lg:text-6xl font-semibold">
+                    Build Engaging UIs with <AnimatedGradientText colorFrom="#D4D4D4" colorTo="#737373" className="text-4xl md:text-5xl lg:text-6xl font-semibold">
                         Animated
                     </AnimatedGradientText> Components
                 </h1>
             </div>
 
-            <div className="mt-5 max-w-3xl text-center mx-auto">
+            <div className="mt-5 max-w-2xl text-center mx-auto">
                 <h2 className="text-lg md:text-xl text-gray-600 dark:text-neutral-400">A collection of ready-to-use UI components built with React, Tailwind CSS, Shadcn, and Motion.</h2>
             </div>
 
