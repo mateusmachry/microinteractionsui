7a1d84d1d0842794794184e33ffd23f2c8203982
Mateus <mateusmachry2016@gmail.com>
2025-06-08 22:11:48 -0300

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans, Noto_Sans_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 
 const notoSans = Noto_Sans({
 	variable: "--font-noto-sans",
@@ -49,6 +50,7 @@ export default function RootLayout({
 				>
 					{children}
 				</ThemeProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
